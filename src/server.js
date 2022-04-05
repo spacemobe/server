3ee88ba04bc821755fbb941b7939cd850b89c626
yuaudio <claudioyuri800@gmail.com>
2022-04-05 12:26:37 +0100

    ola este src/server.js

--- src/server.js
+++ src/server.js
@@ -38,7 +38,57 @@ app.get('/podcasts', (req, res) => {
 				{ name: '64kbps AAC', uri: 'https://radiotungane.ao/radio/8030/radio' },
 				{ name: '48kbps AAC', uri: 'https://radiotungane.ao/radio/8030/mobile' },
 			],
-			programs: createPrograms(),
+			programs: {
+				Dom: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Seg: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Ter: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Qua: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Qui: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Sex: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Sab: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+			},
 		},
 		{
 			id: crypto.randomUUID(),
@@ -52,69 +102,59 @@ app.get('/podcasts', (req, res) => {
 				Dom: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 - 08:00',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '15:15',
-						name: 'Vaga de emprego',
+						hour: '08:00 - 10:00',
+						name: 'K7 dos anos 60/90o',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '16:15',
-						name: 'Aprenda Inglês',
+						hour: '10:00 - 11:00',
+						name: 'Liga pra rádio',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '17:15',
+						hour: '11:00 - 12:00',
+						name: 'Conselhos da Bijú',
+					},
+					{
+						id: crypto.randomUUID(),
+						hour: '12:00 - 13:00',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:15',
-						name: 'Diáspora',
+						hour: '13:00 - 15:00',
+						name: 'Conversa na rádio ',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:45',
-						name: 'Notícia',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '19:00',
-						name: 'Cotidiano',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '2:00',
+						hour: '15:00 - 22:00',
 						name: 'Música',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '21:00',
-						name: 'Jornada Desportiva',
 					},
 				],
 				Seg: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 - 18:15',
 						name: 'Relax',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:15',
+						hour: '18:15 - 18:45',
 						name: 'Juventude Maine',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:45',
+						hour: '18:45 - 19:00',
 						name: 'Notícias',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '19:00',
+						hour: '19:00 - 20:00',
 						name: 'FDMA',
 					},
 					{
@@ -124,81 +164,76 @@ app.get('/podcasts', (req, res) => {
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '21:00',
+						hour: '21:00 - 00:00',
 						name: 'Estação da Música ',
 					},
 				],
 				Ter: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 -18:45',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:45',
+						hour: '18:45: 19:00',
 						name: 'Publicidades',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '19:00',
+						hour: '19:00 - 20:00',
 						name: 'Notícia',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '20:00',
+						hour: '20:00 - 21:00',
 						name: 'Carta na Mesa',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '21:00',
+						hour: '21:00 - 00:00',
 						name: 'Música',
 					},
 				],
 				Qua: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 - 15:15',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '15:15',
-						name: 'Vaga de emprego',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '16:15',
+						hour: '15:15: -17:15',
 						name: 'Aprenda Inglês',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '17:15',
+						hour: '17:15 - 18:15',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:15',
+						hour: '18:15 - 18:45',
 						name: 'Diáspora',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:45',
+						hour: '18:45 - 19:00 ',
 						name: 'Notícia',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '19:00',
+						hour: '19:00 - 20:00',
 						name: 'Cotidiano',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '2:00',
-						name: 'Música',
+						hour: '20:00 - 21:00',
+						name: 'CotidiaMúsicano',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '21:00',
+						hour: '21:00 - 00:00',
 						name: 'Jornada Desportiva',
 					},
 				],
@@ -206,48 +241,33 @@ app.get('/podcasts', (req, res) => {
 				Qui: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 - 19:15',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '15:15',
-						name: 'Vaga de emprego',
+						hour: '15:15 - 18:45',
+						name: 'Candandos/Mainers',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '16:15',
-						name: 'Aprenda Inglês',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '17:15',
-						name: 'Música',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '18:15',
-						name: 'Diáspora',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '18:45',
+						hour: '16:15: 19:00',
 						name: 'Notícia',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '19:00',
-						name: 'Cotidiano',
+						hour: '19:00 - 20:00',
+						name: 'Clube do Rei',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '2:00',
-						name: 'Música',
+						hour: '20:00 - 21:00',
+						name: 'Comédias',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '21:00',
-						name: 'Jornada Desportiva',
+						hour: '21:00 - 000',
+						name: 'Notícia',
 					},
 				],
 				Sex: [
@@ -300,48 +320,48 @@ app.get('/podcasts', (req, res) => {
 				Sab: [
 					{
 						id: crypto.randomUUID(),
-						hour: '00:00',
+						hour: '00:00 - 09: 30',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '15:15',
-						name: 'Vaga de emprego',
+						hour: '09:30 - 10:00',
+						name: 'Conexão Saúde',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '16:15',
-						name: 'Aprenda Inglês',
+						hour: '10:00 - 13:00',
+						name: 'Comando Total',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '17:15',
+						hour: '13:00 - 14:00',
 						name: 'Música',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:15',
-						name: 'Diáspora',
+						hour: '10:00 - 15:00',
+						name: 'Reggae',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '18:45',
-						name: 'Notícia',
+						hour: '15:00 - 16:00',
+						name: 'Recordações',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '19:00',
-						name: 'Cotidiano',
+						hour: '16:00 - 18:00',
+						name: 'FIDMA',
 					},
 					{
 						id: crypto.randomUUID(),
-						hour: '2:00',
+						hour: '18:00 - 20:00',
+						name: 'A conversa com a Beatriz',
+					},
+					{
+						id: crypto.randomUUID(),
+						hour: '20:00 - 20:00',
 						name: 'Música',
-					},
-					{
-						id: crypto.randomUUID(),
-						hour: '21:00',
-						name: 'Jornada Desportiva',
 					},
 				],
 			},
@@ -354,7 +374,57 @@ app.get('/podcasts', (req, res) => {
 				{ name: '64kbps AAC', uri: 'https://radiotungane.ao/radio/8020/radio' },
 				{ name: '48kbps AAC', uri: 'https://radiotungane.ao/radio/8020/mobile' },
 			],
-			programs: createPrograms(),
+			programs: {
+				Dom: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Seg: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Ter: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Qua: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Qui: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Sex: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+				Sab: [
+					{
+						id: crypto.randomUUID(),
+						hour: '00:00 - 23:59',
+						name: 'Música',
+					},
+				],
+			},
 		},
 	]);
 });
